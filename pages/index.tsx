d3be3842d3fbae7932e42f5d8fdbb5042cf98aff
batashof <batashof@gmail.com>
2023-06-27 16:12:56 +0300

    final result

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,138 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {useRouter} from "next/router";
+import {number} from "prop-types";
+import {ChangeEvent} from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+interface IStatus {
+  timestamp: string,
+  error_code: number,
+  error_message: string,
+  elapsed: number,
+  credit_count: number,
+  notice: string
+}
 
-export default function Home() {
+interface IConversion {
+  symbol: string,
+  id: number,
+  name: string,
+  amount: number,
+  last_updated: string,
+  quote: {
+    [number]: {
+      price: number,
+      last_updated:string
+    }
+  }
+}
+interface IConversionResp {
+  data: IConversion,
+  status: IStatus
+}
+
+interface ICurrency {
+  id: number,
+  rank: number,
+  name: string,
+  symbol: string,
+  slug: string,
+  is_active: number,
+  first_historical_data: string,
+  last_historical_data: string,
+  platform: null
+}
+
+interface ICurrencyResp {
+  data: ICurrency,
+  status: IStatus
+}
+
+interface IHomeProps {
+  data: {
+    conversion: IConversion,
+    currencies: ICurrency[]
+  }
+}
+export default function Home({data}: IHomeProps) {
+  const router = useRouter();
+  const id = router.query.id;
+  const convert_id = router.query.convert_id;
+
+  const price = data.conversion?.quote?.[convert_id]?.price;
+  const nameFrom = data.conversion?.name + " (" + data.conversion?.symbol + ")";
+  const nameToObj = data.currencies?.find((item: ICurrency) => item.id?.toString() === (convert_id ?? "1"));
+  const nameTo = nameToObj?.name + " (" + nameToObj?.symbol + ")";
+
+
+  const handleChangeFrom = async (e: ChangeEvent<HTMLSelectElement>) => {
+    await router.push({
+      query: {...router.query, id: e.target.value},
+    })
+  }
+  const handleChangeTo = async (e: ChangeEvent<HTMLSelectElement>) => {
+    await router.push({
+      query: {...router.query, convert_id: e.target.value},
+    })
+  }
+
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex justify-center items-center h-screen">
+      <div className="w-1/2">
+        <h1 className="mb-10 text-center text-lg font-bold">Cryptocurrency Converter</h1>
+        <div className="flex h-fit gap-3">
+          <select id="currency1" onChange={handleChangeFrom} defaultValue={id}
+                  className="bg-gray-50 border border-gray-300 focus-visible:outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            {data.currencies?.map((item: ICurrency) => (
+              <option id={item.id?.toString()} value={item.id} key={item.id}>{item.symbol}</option>
+            ))}
+          </select>
+          <select id="currency2" defaultValue={convert_id} onChange={handleChangeTo}
+                  className="bg-gray-50 border border-gray-300 focus-visible:outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            {data.currencies?.map((item: ICurrency) => (
+              <option id={item.id?.toString()} value={item.id} key={item.id}>{item.symbol}</option>
+            ))}
+          </select>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="text-center mt-5 ">1 {nameFrom} = {price} {nameTo}</div>
       </div>
     </main>
   )
+}
+export const getServerSideProps = async (context) => {
+  const {
+    query: {convert_id = "1", id = "1"},
+  } = context;
+
+  const headers = {
+    'X-CMC_PRO_API_KEY': process.env.X_CMC_PRO_API_KEY,
+    'Access-Control-Allow-Origin': 'http://localhost:3000'
+  }
+
+  const currenciesData = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=30&sort=cmc_rank`, {headers});
+  const currencies: ICurrencyResp = await currenciesData.json();
+  if (!currenciesData.ok) {
+    throw new Error(`Failed to fetch currencies, received status ${currenciesData.status}`)
+  }
+
+  const conversionData = await fetch(`https://pro-api.coinmarketcap.com/v2/tools/price-conversion?amount=1&convert_id=${convert_id}&id=${id}`, {headers})
+  const conversion: IConversionResp = await conversionData.json();
+  if (!conversionData.ok) {
+    throw new Error(`Failed to fetch conversion, received status ${currenciesData.status}`)
+  }
+
+  if (conversion.status.error_code || currencies.status.error_code) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props:
+      {
+        data: {
+          currencies: currencies.data,
+          conversion: conversion.data
+        },
+      }
+  }
 }
